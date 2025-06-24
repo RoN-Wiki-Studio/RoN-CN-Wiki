4c98f5d6fe3588c9903889e325cb1cfea57fe882
tsavpyn <tsavpyn@outlook.com>
2025-06-24 22:50:41 +0800

    init

--- astro.config.ts
+++ astro.config.ts
@@ -10,16 +10,11 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/RoN-Wiki-Studio/RoN-CN-Wiki' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: '内容',
+					autogenerate: {
+						directory: 'wiki'
+					}
+				}
 			],
 			defaultLocale: 'zh-CN',
 			locales: {
